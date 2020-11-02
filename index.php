a3f3db474c3732c383a1fb309647cac0e8cc23d4
Natalya Garaschenko <n.garashchenko@gov39.ru>
2020-11-02 23:28:13 +0200

    Сделана задача по практике основ PHP и консольных приложений https://up.bitrix24.ru/company/personal/user/241/tasks/task/view/599/

--- index.php
+++ index.php
@@ -1,2 +1,39 @@
 <?php
-echo "Hello, GitHub!";
+//Необходимо считать из консоли произвольное*
+// кол-во чисел от пользователя, сложить их и вывести результат.
+$array = [];
+addToArray($array);
+
+function addToArray($array)
+{
+	echo "Введите число: ";
+	$input = trim(fgets(STDIN));
+
+	if(is_numeric($input)) {
+		array_push($array, $input);
+	} else {
+		echo "Введенное значение не является числом" . PHP_EOL;
+	}
+
+	echo "Продолжать вводить? Y/N";
+	$continue = trim(fgets(STDIN));
+
+	checkContinue($continue, $array);
+
+}
+
+function checkContinue($continue, $array)
+{
+	if($continue == "Y")  {
+		echo "Yes" . PHP_EOL;
+		addToArray($array);
+	} elseif($continue == "N") {
+		echo "No" . PHP_EOL;
+		$summa = array_sum($array);
+		echo "Сумма всех введенных значений = $summa";
+	} else {
+		echo "Продолжать вводить? Y/N";
+		$continue = trim(fgets(STDIN));
+		checkContinue($continue, $array);
+	}
+}
