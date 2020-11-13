d82a1e9fcc80f14d65abe27f2e1403649b4a38a6
Natalya Garaschenko <n.garashchenko@gov39.ru>
2020-11-13 23:11:09 +0200

    work#3 сверстала страницу

---
+++ work#3/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+
+<head>
+	<meta charset="utf-8">
+	<title>Главная страница</title>
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE = edge"><![endif]-->
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div class="wrapper">
+	<header class="header">
+		<div class="container container--540">
+			<h2 class="title">Long headline for two lines for attention and evolving</h2>
+			<p class="description">With the new AirPlay 2, you can control your home audio system and the speakers
+				throughout your house.4 You can play a song in the living room and your kitchen at the same time.
+			</p>
+			<div class="payment">
+				<ul class="payment__list">
+					<li class="payment__item"><img src="images/paypal.svg" alt="Paypal"></li>
+					<li class="payment__item"><img src="images/android.svg" alt="Android"></li>
+					<li class="payment__item"><img src="images/ios.svg" alt="IOS"></li>
+					<li class="payment__item"><img src="images/samsungPay.svg" alt="Samsung Pay"></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+	<main class="content">
+		<div class="container">
+			<div class="page-headline">
+				<div class="page-headline__text">
+					<h1 class="title">Medium length display headline</h1>
+					<p class="description">Separated they live in Bookmarks right at the coast of the famous
+						Semantics, large language ocean Separated they live in Bookmarks right </p>
+					<p><a href="#">Action link</a></p>
+				</div>
+				<div class="page-headline__video"></div>
+			</div>
+			<div class="page-content">
+				<h3 class="title">Medium Length Title</h3>
+				<h4 class="title">Medium length headline</h4>
+				<p>By creating an Account on our service, you agree to <a href="#">subscribe to newsletters</a>,
+					marketing or promotional materials and other information we may send. However, you may opt out
+					of receiving any, or all, of these communications from us by following the unsubscribe link or
+					instructions provided in any email we send.</p>
+				<h4 class="title">Medium length headline</h4>
+				<p>However, you may opt out of receiving any, or all, of these communications from us by following
+					the unsubscribe link or instructions provided in any email we send.</p>
+				<p>By creating an Account on our service, you agree to subscribe to newsletters, marketing or
+					promotional materials and other information we may send. However, you may opt out of receiving
+					any, or all, of these communications from us by following the unsubscribe link or instructions
+					provided in any email we send. You understand that your content (<a href="#">not including
+						credit card information</a>), may be transferred unencrypted and involve (a) transmissions
+					over various networks; and (b) changes to conform and adapt to technical requirements of
+					connecting networks or devices. Credit card information is always encrypted during transfer over
+					networks.</p>
+				<ul>
+					<li>Showcase and embed your work with</li>
+					<li>Publish across social channels in a click</li>
+					<li>Sell your videos worldwide</li>
+					<li>Embed your work with</li>
+				</ul>
+			</div>
+		</div>
+	</main>
+</div>
+</body>
+
+</html>
